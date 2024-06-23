70f39c7216ca7386d716c90806b7cdf206b5f1a2
shagost <shagost@hotmail.com>
2024-06-23 22:23:37 +0000

    Correções

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
+import time
 import streamlit as st
 from langchain_openai import OpenAI
 from langchain.chains import ConversationChain  
 from langchain.memory import ConversationBufferMemory
+from langchain.prompts import PromptTemplate
+from langchain.chains.question_answering import load_qa_chain
 from PyPDF2 import PdfReader
-
+import uuid
 from cassandra_db import CassandraDB
 
 openai_api_key = st.secrets["OPENAI_API_KEY"]
 llm = OpenAI(temperature=0)
+sessionId = uuid.uuid4().hex
+memory = ConversationBufferMemory(memory_key="chat_history", input_key="human_input")
+memory.save_context({"human_input": "assistant"}, {"output": "Você é um representante comercial querendo vender um produto ou uma franquia. Lembre todas as perguntas que o humano fizer"})
 
 def generate_response(input_text):
     llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
@@ -15,11 +21,6 @@ def generate_response(input_text):
 
             
 def main(cass_db):
-  conversation = ConversationChain(
-      llm=llm,
-      verbose=True,
-      memory=ConversationBufferMemory()
-  )
   st.title("E.V.A.")
   with st.sidebar.form("form_upload"):
     docs = st.file_uploader("Insira seus arquivos", type="pdf", accept_multiple_files=True)
@@ -27,15 +28,16 @@ def main(cass_db):
       for file in docs:
         text = extract_text(file)
         cass_db.write_vectors_from_text(text, file.name)
+        print(f"Uploaded: {file.name}")
+      print("Uploaded complete!")
   
   if "messages" not in st.session_state:
     st.session_state.messages = []
   
-  conversation.memory = ConversationBufferMemory()
   for message in st.session_state.messages:
     with st.chat_message(message["role"]):
       st.markdown(message["content"])
-    conversation.memory.save_context({"input": message['role']}, {"output": message['content']})
+    memory.save_context({"human_input": message['role']}, {"output": message['content']})
 
   if prompt := st.chat_input("Digite algo..."):
     st.session_state.messages.append({"role": "user", "content": prompt})
@@ -43,12 +45,33 @@ def main(cass_db):
         st.markdown(prompt)
 
     with st.chat_message("assistant"):
-      stream = cass_db.search(prompt)
-      st.session_state.messages.append({"role": "assistant", "content": stream})
-      # conversation.memory.save_context({"input": "assistant"}, {"output": stream})
-      conversation.predict(input=prompt)
-      resp = conversation.memory.buffer_as_messages[-1].content
-      st.write(resp)
+      docs = cass_db.get_documents(prompt)
+      
+      template = """Você é um chatbot conversando com um humano. Se não souber a resposta responda que não sabe.
+
+      Com as partes extraídas dos documentos e a pergunta, crie uma resposta final.
+
+      {context}
+
+      {chat_history}
+      Human: {human_input}
+      Chatbot:"""
+
+      prompt2 = PromptTemplate(
+        input_variables=["chat_history", "human_input", "context"], template=template
+      )
+      chain = load_qa_chain(
+        OpenAI(temperature=0), chain_type="stuff", memory=memory, prompt=prompt2
+      )
+
+      query = prompt
+      result = chain({"input_documents": docs, "human_input": query}, return_only_outputs=True)
+
+      resp = result['output_text'].replace("R$", "R\$")
+      st.markdown(resp)
+      st.session_state.messages.append({"role": "assistant", "content": resp})
+      cass_db.write_questions_from_text(f"Pergunta: {prompt}\nResposta: {resp}", sessionId)
+      print(chain.memory.buffer)
     
 def extract_text(pdf):
   text = ""
@@ -57,8 +80,9 @@ def extract_text(pdf):
       page_text = page.extract_text()
       text += page_text
   return text
-          
+
 if __name__ == '__main__':
   cass = CassandraDB()
+  
   main(cass)
   
--- open_ai.py
+++ open_ai.py
@@ -12,14 +12,23 @@ class Open_AI:
   def __init__(self, db):
     self.db = db
     self.embedding = OpenAIEmbeddings(model='text-embedding-3-small')
-    self.llm = OpenAI()
-    self.text_splitter = CharacterTextSplitter(separator="\n", chunk_size=1000, chunk_overlap=50, length_function=len)
+    self.llm = OpenAI(temperature=0)
+    self.text_splitter = CharacterTextSplitter(separator="\n", chunk_size=1300, chunk_overlap=200, length_function=len)
     
     
   def load_vectors_db(self):
     vstore = Cassandra(
         embedding=self.embedding,
         table_name=self.db.vector_table,
+        keyspace=self.db.keyspace,
+        session=self.db.session
+    )
+    return vstore
+  
+  def load_questions_db(self):
+    vstore = Cassandra(
+        embedding=self.embedding,
+        table_name=self.db.questions_table,
         keyspace=self.db.keyspace,
         session=self.db.session
     )
